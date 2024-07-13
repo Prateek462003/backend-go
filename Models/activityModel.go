1b693130bb0dd53bee00eb8501efddc1271a65ab
Prateek462003 <prateeksaxena462003@gmail.com>
2024-07-13 19:51:32 +0530

    added files

---
+++ Models/activityModel.go
@@ -0,0 +1,6 @@
+package models
+
+type Activity struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Prateek462003/trello-backend/controllers"
+	"github.com/Prateek462003/trello-backend/database"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+
+	database.Init()
+	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		AllowCredentials: true,
+	}))
+
+	router.GET("/activities", controllers.GetActivities)
+	router.POST("/activities", controllers.CreateActivity)
+	router.GET("/activities/:activity_id", controllers.GetTasksByActivity)
+
+	router.GET("/tasks", controllers.GetTasks)
+	router.POST("/tasks", controllers.CreateTask)
+	router.DELETE("/tasks/:id", controllers.DeleteTask)
+
+	port := os.Getenv("PORT")
+	if port == " " {
+		port = "8080"
+	}
+	router.Run("localhost:" + port)
+}
